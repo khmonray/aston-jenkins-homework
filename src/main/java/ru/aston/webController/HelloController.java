b8d3c938f7421664e7c00a7684b019a8376836cf
Ainur Khusnullin <67116700+khmonray@users.noreply.github.com>
2022-12-03 21:07:12 +0500

    add hello world controller and view

--- src/main/java/ru/aston/webController/HelloController.java
+++ src/main/java/ru/aston/webController/HelloController.java
@@ -1,4 +1,14 @@
 package ru.aston.webController;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
 public class HelloController {
+    @GetMapping
+    public String helloPage() {
+        return "/web/hello";
+    }
 }
